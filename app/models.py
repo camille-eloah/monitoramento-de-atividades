c52205fea4ae9ff55ed6f3f8c9aeae5633a7587b
camille-eloah <camelogithub@gmail.com>
2025-01-25 17:29:28 -0300

    Desenvolvimento 5.0

--- app/models.py
+++ app/models.py
@@ -1,41 +1,12 @@
 from flask_login import UserMixin
 
 class Professor(UserMixin):
-    def __init__(self, prof_id, prof_nome, prof_email, prof_senha, prof_tipo):
+    def __init__(self, prof_id, prof_nome, prof_email, prof_senha):
         self.id = prof_id
         self.prof_nome = prof_nome
         self.prof_email = prof_email
         self.prof_senha = prof_senha
 
-class Aluno(UserMixin):
-    def __init__(self, alu_id, alu_nome, alu_matricula, alu_email, alu_curso , alu_data_nasc):
-        self.id = alu_id
-        self.alu_nome = alu_nome
-        self.alu_matricula = alu_matricula
-        self.alu_email = alu_email
-        self.alu_curso = alu_curso
-        self._data_nasc = alu_data_nasc
-
-class Disciplinas(UserMixin):
-    def __init__(self, dis_id, dis_nome,dis_prof_responsavel,dis_carga_hr):
-        self.id = dis_id
-        self.dis_nome = dis_nome
-        self.dis_prof_responsavel = dis_prof_responsavel
-        self.dis_carga_hr = dis_carga_hr
-
-
-class Atividades(UserMixin):
-    def __init__(self, ati_id, ati_tipo, ati_descricao, ati_data_entr, ati_peso):
-        self.id = ati_id
-        self.ati_tipo = ati_tipo
-        self.ati_descricao = ati_descricao
-        self.ati_data_entr = ati_data_entr
-        self.ati_peso = ati_peso
-
-class Frequencias(UserMixin):
-    def __init__(self, fre_id, fre_chamada):
-        self.id = fre_id
-        self.fre_chamada = fre_chamada
 
     '''@staticmethod
     def get(user_id):
--- init_db.sql
+++ init_db.sql
@@ -22,13 +22,13 @@ create table if not exists tb_professores (
     prof_id integer auto_increment primary key,
     prof_nome VARCHAR(50) not null,
     prof_email VARCHAR(100) not null unique,
-    prof_senha VARCHAR(50) not nul
+    prof_senha VARCHAR(50) not null
 
 );
 
 create table if not exists tb_aulas (
     aul_id integer auto_increment primary key,
-    ati_descricao VARCHAR(200) not null,
+    aul_descricao VARCHAR(200) not null,
     aul_data datetime not null,
     foreign key (aul_id) references tb_professores(prof_id),
     foreign key (aul_id) references tb_disciplinas(dis_id)
@@ -63,4 +63,6 @@ create table if not exists tb_frequencia (
     foreign key (dis_id) references tb_disciplinas(dis_id),
     foreign key (alu_id) references tb_alunos(alu_id) 
 );
+
+
 
