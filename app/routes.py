5f272713f8d3e935f089c88ad3d8c821c6023bb5
camille-eloah <camelogithub@gmail.com>
2025-01-28 15:29:28 -0300

    Registro de entrega de atividades funcionando

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, request, redirect, url_for, flash
 from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 from app import app, get_db_connection
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 import pymysql
 from pymysql.err import IntegrityError 
@@ -539,9 +540,7 @@ def cad_atividades():
     finally:
         connection.close()
 
-    return render_template(
-        'atividades/cad_atividades.html', atividades=atividades, disciplinas=disciplinas
-    )
+    return render_template('atividades/cad_atividades.html', atividades=atividades, disciplinas=disciplinas)
 
 
 
@@ -911,6 +910,7 @@ def relatorios():
 
         cursor.execute('SELECT * FROM tb_aluno_atividade')
         alu_atividade = cursor.fetchall()
+        print(alu_atividade)
 
         # Consultar o relatório de faltas por aluno
         cursor.execute("""
@@ -959,23 +959,38 @@ def registro_entrega(ati_id):
     connection = get_db_connection()
 
     if request.method == 'POST':
-        # Registrar entrega de cada aluno
         for aluno_id in request.form.getlist('aluno_id'):
             situacao = request.form.get(f'situacao_{aluno_id}')
             nota = request.form.get(f'nota_{aluno_id}')
-            if situacao and nota:
-                query = """
-                    INSERT INTO tb_aluno_atividade (alunoativ_alu_id, alunoativ_ati_id, alunoativ_situacao, alunoativ_nota)
-                    VALUES (%s, %s, %s, %s)
+            data_entrega = request.form.get(f'data_entrega_{aluno_id}')
+            if situacao and nota and data_entrega:
+                query_check = """
+                    SELECT alunoativ_id
+                    FROM tb_aluno_atividade
+                    WHERE alunoativ_alu_id = %s AND alunoativ_ati_id = %s
                 """
-                executar_query(query, (aluno_id, ati_id, situacao, float(nota)))
+                with connection.cursor() as cursor:
+                    cursor.execute(query_check, (aluno_id, ati_id))
+                    existing_record = cursor.fetchone()
+
+                if existing_record:
+                    query_update = """
+                        UPDATE tb_aluno_atividade
+                        SET alunoativ_situacao = %s, alunoativ_nota = %s, alunoativ_data_entrega = %s
+                        WHERE alunoativ_alu_id = %s AND alunoativ_ati_id = %s
+                    """
+                    executar_query(query_update, (situacao, float(nota), data_entrega, aluno_id, ati_id))
+                else:
+                    query_insert = """
+                        INSERT INTO tb_aluno_atividade (alunoativ_alu_id, alunoativ_ati_id, alunoativ_situacao, alunoativ_nota, alunoativ_data_entrega)
+                        VALUES (%s, %s, %s, %s, %s)
+                    """
+                    executar_query(query_insert, (aluno_id, ati_id, situacao, float(nota), data_entrega))
 
         flash("Entregas registradas com sucesso!", "success")
         return redirect(f'/registro_entrega/{ati_id}')
 
-    # Buscar dados para exibir
     with connection.cursor() as cursor:
-        # Buscar informações da atividade
         cursor.execute("""
             SELECT ati_tipo, ati_descricao, ati_data_entrega
             FROM tb_atividades
@@ -983,13 +998,22 @@ def registro_entrega(ati_id):
         """, (ati_id,))
         atividade = cursor.fetchone()
 
-        # Buscar alunos matriculados na disciplina da atividade
         cursor.execute("""
-            SELECT a.alu_id, a.alu_nome
+            SELECT a.alu_id, a.alu_nome, 
+                   COALESCE(aa.alunoativ_situacao, 'Em andamento') AS situacao,
+                   COALESCE(aa.alunoativ_nota, 0) AS nota,
+                   COALESCE(aa.alunoativ_data_entrega, NULL) AS data_entrega
             FROM tb_alunos a
             JOIN tb_alunos_disciplinas ad ON a.alu_id = ad.ad_alu_id
+            LEFT JOIN tb_aluno_atividade aa 
+                   ON a.alu_id = aa.alunoativ_alu_id AND aa.alunoativ_ati_id = %s
             WHERE ad.ad_dis_id = (SELECT ati_dis_id FROM tb_atividades WHERE ati_id = %s)
-        """, (ati_id,))
+        """, (ati_id, ati_id))
         alunos = cursor.fetchall()
+
+    # Formatar a data de entrega no formato necessário
+    for aluno in alunos:
+        if aluno['data_entrega']:
+            aluno['data_entrega'] = aluno['data_entrega'].strftime('%Y-%m-%dT%H:%M')
 
     return render_template('atividades/registro_entrega.html', atividade=atividade, alunos=alunos, ati_id=ati_id)
